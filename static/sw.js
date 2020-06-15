043f6b4869ffefebc33fd0e4d8d3de0985a9e58d
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2020-06-15 19:57:20 -0300

    service worker updateed

--- static/sw.js
+++ static/sw.js
@@ -6,6 +6,50 @@ var preLoad = function () {
   console.log("Installing web app");
   return caches.open("offline").then(function (cache) {
     console.log("caching index and important routes");
-    return cache.addAll(["/"]);
+    return cache.addAll(["/", "/offline.html"]);
+  });
+};
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(checkResponse(event.request).catch(function () {
+    return returnFromCache(event.request);
+  }));
+  event.waitUntil(addToCache(event.request));
+});
+
+var checkResponse = function (request) {
+  return new Promise(function (fulfill, reject) {
+    fetch(request).then(function (response) {
+      if (response.status !== 404) {
+        fulfill(response);
+      } else {
+        reject();
+      }
+    }, reject);
+  });
+};
+
+var addToCache = function (request) {
+  try {
+    return caches.open("offline").then(function (cache) {
+      return fetch(request).then(function (response) {
+        console.log(response.url + " was cached");
+        return cache.put(request, response);
+      });
+    });
+  } catch {
+    console.log('Cound not addToCache');
+  }
+};
+
+var returnFromCache = function (request) {
+  return caches.open("offline").then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if ((!matching || matching.status == 404) && request.method != 'HEAD') {
+        return cache.match("offline.html");
+      } else {
+        return matching;
+      }
+    });
   });
 };
