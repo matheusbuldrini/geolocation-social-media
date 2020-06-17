ca2b992ba1d72678237c60eb3c0f42503bb584ac
matheusbuldrini <38816666+matheusbuldrini@users.noreply.github.com>
2020-06-17 11:10:00 -0300

    Update config.py

--- config.py
+++ config.py
@@ -9,6 +9,6 @@ DEBUG = True
 SECRET_KEY = ''.join(random.choice(string.ascii_letters) for i in range(42))
 
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite')
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://fqbm52xovhg5aqvg:dwa2nj7iana6cei1@un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kpwsnw49ild5dw7z'
+SQLALCHEMY_DATABASE_URI = os.environ['JAWSDB_MARIA_URL']
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
